f303784d478ce571e14c30979a59c30c1490cc62
tazsol <tzsolt92@gmail.com>
2022-01-12 10:18:40 +0100

    initial config
    initial config

---
+++ Assets/Scripts/GameHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+public class GameHandler : MonoBehaviour {
+
+    private void Start() {
+        Debug.Log("GameHandler.Start");
+    }
+
+}
